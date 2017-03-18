4197ae2c86f3881f65d3e0538e452e1c876fb938
DhavalPatel1988 <dhaval920@gmail.com>
2017-03-18 18:03:34 +0000

    product and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318141128) do
+ActiveRecord::Schema.define(version: 20170318180054) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
